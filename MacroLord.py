5c7aa9876e39f64b73851cfaa31ccb1b64875c49
lete <103456882+LETEX007@users.noreply.github.com>
2023-01-04 16:24:01 -0300

    Update MacroLord.py

--- MacroLord.py
+++ MacroLord.py
@@ -1,1 +1,21 @@
-
+from ppadb.client import Client as AdbClient
+import numpy as np
+import pyautogui as py
+#from pytesseract import *
+import io
+import os
+import cv2
+import PIL.Image as Image
+from PIL import ImageOps
+import keyboard
+import time
+import random
+import pytesseract #C:\Program Files\Tesseract-OCR\tesseract.exe
+import sys
+import threading
+import sqlite3
+from sqlite3 import Error
+import ctypes
+import subprocess
+from datetime import datetime
+import requests
